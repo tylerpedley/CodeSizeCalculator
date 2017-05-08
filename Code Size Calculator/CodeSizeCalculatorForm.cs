1e1d5a6eb6a2794b752505a628dce352d74037d1
Tyler <tyler.pedley@bluefruit.co.uk>
2017-05-08 23:26:51 +0100

    Basic form with upload file button and dialog and displays contents of file

---
+++ Code Size Calculator/CodeSizeCalculatorForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Code_Size_Calculator
+{
+    public partial class CodeSizeCalculatorForm : Form
+    {
+        private StreamReader FileUploadStream;
+
+        public CodeSizeCalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        ~CodeSizeCalculatorForm()
+        {
+            FileUploadStream.Close();
+        }
+
+        private async void UploadFileButton_Click(object sender, EventArgs e)
+        {
+            if (UploadFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileUploadStream = new StreamReader(UploadFileDialog.FileName);
+                Task<string> calculateCodeSizeTask = new Task<string>(CalculateCodeSize);
+                calculateCodeSizeTask.Start();
+
+                var codeSize = await calculateCodeSizeTask;
+
+                MessageBox.Show(codeSize);
+            }
+        }
+
+        private string CalculateCodeSize()
+        {
+            var fileContents = FileUploadStream.ReadToEnd();
+
+            return fileContents;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Code Size Calculator/Program.cs
+++ Code Size Calculator/Program.cs
@@ -16,7 +16,7 @@ namespace Code_Size_Calculator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CodeSizeCalculatorForm());
         }
     }
 }
